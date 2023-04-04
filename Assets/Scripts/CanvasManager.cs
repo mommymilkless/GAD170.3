60b0f9e2dbbd6574db926555bd84846707cb1093
mommymilkless <finlayson.ariana@gmail.com>
2023-04-04 15:09:08 +1000

    Some extra scripts I'll need and whatnot

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class CanvasManager : MonoBehaviour
 {
-    // Awake is called before Start
-    void Awake()
-    {
-        
-    }
-    
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        
     }
 
     // Update is called once per frame
