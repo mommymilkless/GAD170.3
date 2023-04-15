56b35d57d9ed446c1472bd8ad9e453100b5e2896
mommymilkless <finlayson.ariana@gmail.com>
2023-04-15 19:41:26 +1000

    Minor changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Awake is called before Start
-    void Awake()
-    {
-        
-    }
-    
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        //Debug.Log("Cursor is Locked");
     }
 
     // Update is called once per frame
