6771490c299ed7d8ea9694bbba3f3106060dae06
mommymilkless <finlayson.ariana@gmail.com>
2023-04-18 15:07:54 +1000

    Pain

--- Assets/Scripts/TriggerTime.cs
+++ Assets/Scripts/TriggerTime.cs
@@ -4,25 +4,52 @@ using UnityEngine;
 
 public class TriggerTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // THIS SCRIPT NEEDS
+    /*
+        To trigger tutorial pop-ups - call to the canvas manager lolol
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        }
 
+        To trigger a vignette of some sort to indicate to the player they've been spotted on trigger stay - canvas manager
+        {
+
+        }
+
+        An area that triggers the ability to use a something/press a button (this will be the hat shop analogy) - game controller
+        {
+
+        }
+    */
+
+    // Called when you first enter a trigger volume
     void OnTriggerEnter(Collider collision)
     {
-        
+        // Display tutorial pop-ups
+        if(collision.gameObject.tag=="Player")
+        {
+            // If statement to check if the thing the player is colliding with is the tutorial zone or the cat at the end
+            // Call to canvas manager for tutorial pop-ups
+        }
     }
 
+    // Called while you're inside the trigger volume
     void OnTriggerStay(Collider collision)
     {
+        // Losing obscurity while inside the trigger volume
+        if(collision.gameObject.tag=="Player")
+        {
+            // Lose obscurity
+            // Call to the canvas manager to trigger vignette thingy
+        }
+
+        // Vignette is on screen - maybe gets more intense??
+        
+        // ability to use the hats
+
+        // ability to consult with the overlord
+        
+        
         // From Week 9 code
         /*
         if(collision.gameObject.name=="Player")
